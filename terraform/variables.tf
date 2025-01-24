4fd10d8bc15a6413bbc26c8ee8e0cfbd5d2f928a
Aditya Keshav <adityakeshav393@gmail.com>
2025-01-24 23:49:20 +0530

    var

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,7 @@ variable "docker_image" {
   description = "The Docker image to deploy to Azure Container Instance"
   type        = string
 }
+variable "subscription_id" {
+  description = "The Azure subscription ID"
+  type        = string
+}
